e761372de25aebee7474d8d6500e7dd63bc43191
tanvir <mstanvirislam05@gmail.com>
2026-08-18 17:30:49 +0600

    feat: implement CRUD endpoints for songs

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
 export class SongsController {
+  @Post()
+  post() {
+    return 'create a song endpoint';
+  }
+
   @Get()
   findAll() {
-    return 'find all songs';
+    return 'find all songs endpoints';
+  }
+  @Get(':id')
+  findOne() {
+    return 'fetch song based on id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song based on id';
   }
 }
